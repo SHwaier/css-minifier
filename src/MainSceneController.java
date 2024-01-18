3c8d753d642832e785bdaf79c4b1c6d8cbf8a2cc
Unknown159 <59855335+SHwaier@users.noreply.github.com>
2024-01-18 03:26:14 -0500

    Javafx app structure setup

---
+++ src/MainSceneController.java
@@ -0,0 +1,23 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class MainSceneController {
+
+    @FXML
+    private TextArea OriginalText;
+
+    @FXML
+    private TextArea OutputText;
+
+    @FXML
+    void btnCopyClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOpenClicked(ActionEvent event) {
+
+    }
+
+}
